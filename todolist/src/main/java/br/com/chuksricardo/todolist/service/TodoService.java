68f1ec6962658f565e289036d4d664c8ce90bec6
Chuks Ricardo Iloegbunam <chuksricardo@gmail.com>
2025-05-24 02:42:23 -0300

    Definição do dominio - service

---
+++ todolist/src/main/java/br/com/chuksricardo/todolist/service/TodoService.java
@@ -0,0 +1,46 @@
+package br.com.chuksricardo.todolist.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import br.com.chuksricardo.todolist.entity.Todo;
+import br.com.chuksricardo.todolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+  private TodoRepository todoRepository;
+
+  public TodoService(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+
+
+  public List<Todo> create(Todo todo){
+    todoRepository.save(todo);
+    return list();
+  }
+
+
+  public List<Todo> list(){
+    Sort sort = Sort.by("prioridade").descending().and(
+      Sort.by("nome").ascending()
+    );
+    return todoRepository.findAll(sort);
+  }
+  
+  
+  public List<Todo> update(Todo todo){
+    todoRepository.save(todo);
+    return list();    
+  }
+  
+  
+  public List<Todo> delete(Long id){
+    todoRepository.deleteById(id);
+    return list();    
+  }
+
+}
